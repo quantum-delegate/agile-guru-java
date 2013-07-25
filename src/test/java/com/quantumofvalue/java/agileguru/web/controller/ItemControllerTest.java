604f8862784de81bde1fab17862e3f77749599e1
unknown <chris.turns@INPSESPC113.is.inps.co.uk>
2013-07-25 16:09:48 +0100

    the final stage

--- src/test/java/com/quantumofvalue/java/agileguru/web/controller/ItemControllerTest.java
+++ src/test/java/com/quantumofvalue/java/agileguru/web/controller/ItemControllerTest.java
@@ -1,20 +1,65 @@
-
 package com.quantumofvalue.java.agileguru.web.controller;
- 
+
 import static org.junit.Assert.assertEquals;
+
+import java.util.Arrays;
+import java.util.List;
+
 import static org.junit.Assert.assertNotNull;
+
+import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
+import org.springframework.test.util.ReflectionTestUtils;
 import org.springframework.ui.ExtendedModelMap;
+
+import com.quantumofvalue.java.agileguru.domain.Item;
+import com.quantumofvalue.java.agileguru.service.ItemService;
+
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
 public class ItemControllerTest extends AbstractControllerTest {
-     
-    @Test
-    public void testItemControllerReturnsTheCorrectView() throws Exception {
-         
-        ItemController itemController = new ItemController();
-         
-        ExtendedModelMap uiModel = new ExtendedModelMap();
-        String result = itemController.list(uiModel);
-        assertNotNull(result);
-        assertEquals("items/list", result );
-    }   
+
+	private ItemService itemService;
+	private List<Item> items;
+	ItemController itemController ;
+
+	@Before
+	public void setUp() {
+
+		items = Arrays.asList(new Item("Item 1"), new Item("Item 2"));
+	    itemController = new ItemController();
+	    itemService = mock(ItemService.class);
+
+		when(itemService.findAll()).thenReturn(items);
+
+		
+		ReflectionTestUtils
+				.setField(itemController, "itemService", itemService);
+	}
+
+	@Test
+	public void testItemControllerReturnsTheCorrectView() throws Exception {
+
+		ExtendedModelMap uiModel = new ExtendedModelMap();
+		String result = itemController.list(uiModel);
+		assertNotNull(result);
+		assertEquals("items/list", result);
+	}
+
+	@Test
+	public void testItemControllerRetrievsTheItemsFromTheDatabase()
+			throws Exception {
+		
+		ExtendedModelMap uiModel = new ExtendedModelMap();
+
+		itemController.list(uiModel);
+
+		@SuppressWarnings("unchecked")
+		List<Item> modelItems = (List<Item>) uiModel.get("items");
+
+		Assert.assertNotNull(modelItems);
+		Assert.assertArrayEquals(items.toArray(), modelItems.toArray());
+	}
 }
