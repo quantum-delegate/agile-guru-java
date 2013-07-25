d50d2908b88d45250735b3f788e8d3e7d7cf1862
unknown <chris.turns@INPSESPC113.is.inps.co.uk>
2013-07-25 13:36:03 +0100

    Initial commit

---
+++ src/test/resources/com/quantumofvalue/java/agileguru/cucumber/features/WalkingSkeletonFeature.java
@@ -0,0 +1,9 @@
+package com.quantumofvalue.java.agileguru.cucumber.features;
+ 
+import org.junit.runner.RunWith;
+import cucumber.api.junit.Cucumber;
+@RunWith(Cucumber.class)
+@Cucumber.Options(features = "classpath:com/quantumofvalue/java/agileguru/cucumber/features/WalkingSkeleton.feature",
+                  glue="com.quantumofvalue.java.agileguru.cucumber.features.stepdefinitions")
+public class WalkingSkeletonFeature {
+}
