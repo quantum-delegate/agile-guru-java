98a56bda0e8ea41dc86eb7fd6e2a14df3223777a
lyall donaldson <lyall.donaldson@inps.co.uk>
2013-07-25 16:07:39 +0100

    Bug fixing :(

--- src/test/java/com/quantumofvalue/java/agileguru/cucumber/stepdefinitions/WalkingSkeletonSteps.java
+++ src/test/java/com/quantumofvalue/java/agileguru/cucumber/stepdefinitions/WalkingSkeletonSteps.java
@@ -1,5 +1,7 @@
 package com.quantumofvalue.java.agileguru.cucumber.stepdefinitions;
 
+import static com.google.common.collect.Lists.transform;
+
 import java.util.Arrays;
 import java.util.List;
 import java.util.concurrent.TimeUnit;
@@ -15,11 +17,10 @@ import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 import com.google.common.base.Function;
-import static com.google.common.collect.Lists.transform;
 import com.quantumofvalue.java.agileguru.domain.Item;
 import com.quantumofvalue.java.agileguru.service.ItemService;
 
-import cucumber.api.PendingException;
+import cucumber.api.java.After;
 import cucumber.api.java.Before;
 import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
@@ -45,6 +46,11 @@ public class WalkingSkeletonSteps {
 		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
 	}
 
+	@After
+	public void afterScenario() {
+		driver.close();
+	}
+
 	@Given("^items \"([^\"]*)\" and \"([^\"]*)\" in the database$")
 	public void items_and_in_the_database(String item1, String item2)
 			throws Throwable {
@@ -65,13 +71,17 @@ public class WalkingSkeletonSteps {
 	}
 
 	@Then("^I should see \"([^\"]*)\" and \"([^\"]*)\" listed on the page.$")
-	public void I_should_see_and_listed_on_the_page(String item1, String item2) throws Throwable {
-	    List<WebElement> tdElements = driver.findElements(By.xpath("//td[@class='item_text']"));
-	    List<String> stringElements = transform(tdElements, new Function<WebElement,String>() {
-	        public String apply(WebElement webElement) {
-	            return webElement.getText();
-	        }
-	    });
-	    Assert.assertArrayEquals(new String[] {item1, item2}, stringElements.toArray());
+	public void I_should_see_and_listed_on_the_page(String item1, String item2)
+			throws Throwable {
+		List<WebElement> tdElements = driver.findElements(By
+				.xpath("//td[@class='item_text']"));
+		List<String> stringElements = transform(tdElements,
+				new Function<WebElement, String>() {
+					public String apply(WebElement webElement) {
+						return webElement.getText();
+					}
+				});
+		Assert.assertArrayEquals(new String[] { item1, item2 },
+				stringElements.toArray());
 	}
-	}
+}
