7b908cd3ac204ddd0cb789c32b546275f5dd3cf0
lyall donaldson <lyall.donaldson@inps.co.uk>
2013-07-25 14:04:38 +0100

    Episode_2

---
+++ src/main/java/com/quantumofvalue/java/agileguru/repository/ItemRepository.java
@@ -0,0 +1,8 @@
+package com.quantumofvalue.java.agileguru.repository;
+ 
+import org.springframework.data.repository.CrudRepository;
+import com.quantumofvalue.java.agileguru.domain.Item;
+ 
+public interface ItemRepository extends CrudRepository<Item, Long> {
+     
+}
