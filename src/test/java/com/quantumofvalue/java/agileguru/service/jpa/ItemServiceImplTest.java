0da4fb082d4a787014645630eb31b9ae0b101ef7
unknown <michael.lindsay@inpsespc110.is.inps.co.uk>
2013-07-25 14:23:05 +0100

    Tumbleweed Gooooo 2!

---
+++ src/test/java/com/quantumofvalue/java/agileguru/service/jpa/ItemServiceImplTest.java
@@ -0,0 +1,29 @@
+package com.quantumofvalue.java.agileguru.service.jpa;
+ 
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+ 
+import java.util.List;
+ 
+import org.junit.Test;
+ 
+import org.springframework.beans.factory.annotation.Autowired;
+import com.quantumofvalue.java.agileguru.domain.Item;
+import com.quantumofvalue.java.agileguru.service.ItemService;
+ 
+import com.quantumofvalue.java.agileguru.test.annotation.DataSets;
+ 
+public class ItemServiceImplTest extends AbstractServiceImplTest {
+ 
+    @Autowired
+    ItemService itemService;
+     
+    @DataSets(setUpDataSet="/com/quantumofvalue/java/agileguru/service/jpa/ItemServiceImplTest.xls")
+    @Test
+    public void testFindAll()
+        throws Exception {
+        List<Item> result = itemService.findAll();
+        assertNotNull(result);
+        assertEquals(2, result.size());
+    }
+}
