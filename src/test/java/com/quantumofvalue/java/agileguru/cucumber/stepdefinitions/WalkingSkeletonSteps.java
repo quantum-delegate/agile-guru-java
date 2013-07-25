cf8dab8232d92f724fe333b83b6468c973856fd7
unknown <michael.lindsay@inpsespc110.is.inps.co.uk>
2013-07-25 15:02:24 +0100

    Tumbleweed FIX!!

--- src/test/java/com/quantumofvalue/java/agileguru/cucumber/stepdefinitions/WalkingSkeletonSteps.java
+++ src/test/java/com/quantumofvalue/java/agileguru/cucumber/stepdefinitions/WalkingSkeletonSteps.java
@@ -3,15 +3,19 @@ package com.quantumofvalue.java.agileguru.cucumber.stepdefinitions;
  
 import java.util.Arrays;
 import java.util.List;
+
 import javax.annotation.Resource;
+
 import org.junit.Assert;
+import org.openqa.selenium.WebDriver;
+
 import com.quantumofvalue.java.agileguru.domain.Item;
 import com.quantumofvalue.java.agileguru.service.ItemService;
+
 import cucumber.api.java.Before;
 import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
-import cucumber.api.PendingException;
  
 public class WalkingSkeletonSteps {
      
@@ -20,10 +24,14 @@ public class WalkingSkeletonSteps {
      
     @Resource(name="databaseCleaner")
     DatabaseCleaner databaseCleaner;
+    
+    WebDriver driver;
      
     @Before
     public void beforeScenario() {  
         databaseCleaner.truncate();
+        
+        //driver = new ChromeDriver();
     }
     @Given("^items \"([^\"]*)\" and \"([^\"]*)\" in the database$")
     public void items_and_in_the_database(String item1, String item2) throws Throwable {
@@ -34,14 +42,15 @@ public class WalkingSkeletonSteps {
          
         Assert.assertArrayEquals(items.toArray(), itemService.findAll().toArray());
     }
+    
     @When("^I open \"([^\"]*)\" page$")
     public void I_open_page(String arg1) throws Throwable {
         // Express the Regexp above with the code you wish you had
-        throw new PendingException();
+        throw new RuntimeException();
     }
     @Then("^I should see \"([^\"]*)\" and \"([^\"]*)\" listed on the page.$")
     public void I_should_see_and_listed_on_the_page(String arg1, String arg2) throws Throwable {
         // Express the Regexp above with the code you wish you had
-        throw new PendingException();
+        throw new RuntimeException();
     }
 }
