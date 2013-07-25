b6e0ece9712abd27f3ab0c916ddfd47bef6b000e
unknown <chris.turns@INPSESPC113.is.inps.co.uk>
2013-07-25 15:14:56 +0100

    Steeeep 4

--- src/test/resources/com/quantumofvalue/java/agileguru/cucumber/features/stepdefinitions/WalkingSkeletonSteps.java
+++ src/test/resources/com/quantumofvalue/java/agileguru/cucumber/features/stepdefinitions/WalkingSkeletonSteps.java
@@ -3,8 +3,15 @@ package com.quantumofvalue.java.agileguru.cucumber.features.stepdefinitions;
  
 import java.util.Arrays;
 import java.util.List;
+import java.util.concurrent.TimeUnit;
+
 import javax.annotation.Resource;
 import org.junit.Assert;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
 import com.quantumofvalue.java.agileguru.domain.Item;
 import com.quantumofvalue.java.agileguru.service.ItemService;
 import cucumber.api.java.Before;
@@ -20,11 +27,19 @@ public class WalkingSkeletonSteps {
      
     @Resource(name="databaseCleaner")
     DatabaseCleaner databaseCleaner;
-     
+    
+    WebDriver driver;
+    
+    private static final String baseUrl = "http://localhost:8080";
+    
     @Before
     public void beforeScenario() {  
         databaseCleaner.truncate();
+             
+        driver = new FirefoxDriver();
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
     }
+    
     @Given("^items \"([^\"]*)\" and \"([^\"]*)\" in the database$")
     public void items_and_in_the_database(String item1, String item2) throws Throwable {
          
@@ -35,9 +50,10 @@ public class WalkingSkeletonSteps {
         Assert.assertArrayEquals(items.toArray(), itemService.findAll().toArray());
     }
     @When("^I open \"([^\"]*)\" page$")
-    public void I_open_page(String arg1) throws Throwable {
-        // Express the Regexp above with the code you wish you had
-        throw new PendingException();
+    public void I_open_page(String url) throws Throwable {
+        driver.get(baseUrl+url);
+        WebDriverWait wait = new WebDriverWait(driver, 10);
+        wait.until(ExpectedConditions.titleIs("Agile Guru - Items"));
     }
     @Then("^I should see \"([^\"]*)\" and \"([^\"]*)\" listed on the page.$")
     public void I_should_see_and_listed_on_the_page(String arg1, String arg2) throws Throwable {
